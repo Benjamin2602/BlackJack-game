06f511164f2a8960bb045c4fc6831f58be2c9d36
Benjamin2602 <91728550+Benjamin2602@users.noreply.github.com>
2023-05-29 10:52:32 +0530

    fixed the bug

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function startGame() {
 }
 
 function renderGame() {
-  cardEl.textContent = "Cards: " + cards[0] + " " + cards[1];
+  cardEl.textContent = "Cards: ";
   
   for (let i = 0; i < cards.length; i++) {
     cardEl.textContent += cards[i] + " ";
